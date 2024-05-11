cc23fcfae6f1bf8c56410fe079d5a15c7082ea63
sopheak012 <36690234+sopheak012@users.noreply.github.com>
2024-05-11 12:22:33 -0700

    update search bar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,15 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addGuessedPokemon } from "../Redux/Slices/pokemonSlice";
+import {
+  addGuessedPokemon,
+  setGameWon,
+  resetGame,
+} from "../Redux/Slices/pokemonSlice";
 import PokemonData from "../data/gen1.json";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
-  const [invalidInput, setInvalidInput] = useState(false); // State for invalid input
+  const [invalidInput, setInvalidInput] = useState(false);
   const searchBarRef = useRef(null);
   const dispatch = useDispatch();
   const guessedPokemon = useSelector((state) => state.pokemon.guessedPokemon);
+  const isGameWon = useSelector((state) => state.pokemon.isGameWon);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -66,6 +71,15 @@ const SearchBar = () => {
       if (isValidPokemon && !isAlreadyGuessed) {
         dispatch(addGuessedPokemon(searchTerm));
         setSearchTerm("");
+
+        // Check if all Pokemon have been guessed
+        const allPokemonGuessed = PokemonData.every((pokemon) =>
+          guessedPokemon.includes(pokemon.name)
+        );
+
+        if (allPokemonGuessed) {
+          dispatch(setGameWon(true));
+        }
       } else if (isAlreadyGuessed) {
         setInvalidInput(true);
         setSearchTerm("");
@@ -76,73 +90,86 @@ const SearchBar = () => {
     }
   };
 
+  const handleNewGame = () => {
+    dispatch(resetGame());
+  };
+
   return (
-    <form className="max-w-md mx-auto pt-64" onSubmit={handleGuess}>
-      {invalidInput && (
-        <p className="text-red-500 mb-2 text-sm text-center">
-          Invalid Pokémon name
-        </p>
-      )}
-      <label
-        htmlFor="default-guess"
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >
-        Guess
-      </label>
-      <div ref={searchBarRef} className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
-        </div>
-        <input
-          type="text"
-          id="default-guess"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Guess a Pokemon"
-          value={searchTerm}
-          onChange={handleChange}
-        />
-        {suggestions.length > 0 && searchTerm !== "" && (
-          <div className="absolute z-10 w-full mt-1 max-h-48 overflow-y-auto bg-white rounded-lg border border-gray-300 shadow-md">
-            <ul>
-              {suggestions.map((suggestion, index) => (
-                <li
-                  key={index}
-                  className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100"
-                  onClick={() => handleSuggestionClick(suggestion)}
-                >
-                  <img
-                    src={suggestion.image}
-                    alt={suggestion.name}
-                    className="w-8 h-8 mr-2"
-                  />
-                  {suggestion.name}
-                </li>
-              ))}
-            </ul>
-          </div>
+    <div className="max-w-md mx-auto pt-64 flex">
+      <form className="relative flex-grow" onSubmit={handleGuess}>
+        {invalidInput && (
+          <p className="text-red-500 mb-2 text-sm text-center">
+            Invalid Pokémon name
+          </p>
         )}
-        <button
-          type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <label
+          htmlFor="default-guess"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
         >
           Guess
-        </button>
-      </div>
-    </form>
+        </label>
+        <div ref={searchBarRef} className="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="default-guess"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Guess a Pokemon"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          {suggestions.length > 0 && searchTerm !== "" && (
+            <div className="absolute z-10 w-full mt-1 max-h-48 overflow-y-auto bg-white rounded-lg border border-gray-300 shadow-md">
+              <ul>
+                {suggestions.map((suggestion, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100"
+                    onClick={() => handleSuggestionClick(suggestion)}
+                  >
+                    <img
+                      src={suggestion.image}
+                      alt={suggestion.name}
+                      className="w-8 h-8 mr-2"
+                    />
+                    {suggestion.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          <button
+            type="submit"
+            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Guess
+          </button>
+        </div>
+      </form>
+      <button
+        type="button"
+        className="bg-blue-700 hover:bg-blue-800 text-white font-bold p-2 rounded ml-4 m-2"
+        onClick={handleNewGame}
+      >
+        New Game
+      </button>
+    </div>
   );
 };
 
